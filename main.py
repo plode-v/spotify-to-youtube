be7e5535d9b78b7b5ca923d99031b536b56b9be0
Plode <84858705+plode-v@users.noreply.github.com>
2023-03-21 21:47:42 +0700

    refractor code to be in class-based code in main.py

--- main.py
+++ main.py
@@ -2,23 +2,31 @@ from spotify import Spotify
 from youtube_id import YoutubeId
 from dotenv import load_dotenv
 import os
+import re
 
 # Load_dotenv function is execute for local environment variables
 load_dotenv()
 
-spotify = Spotify()
-youtube_id = YoutubeId()
+class App:
+    def __init__(self):
+        self.spotify = Spotify()
+        self.youtube_id = YoutubeId()
+        self.client_id = os.environ["CLIENT_ID"] #Replace with your Spotify Client ID
+        self.client_secret = os.environ["CLIENT_SECRET"] # Replace with your Spotify CLient Secret
+        self.developer_key = os.environ["YOUTUBE_API_KEY"] # Replace with your YouTube API Key
+        self.playlist_link = "https://open.spotify.com/playlist/7FaRRy07GXHSF4c4CKJSaP?si=3942427326c14b2a" # Replace with your own Spotify Playlist's Link
+        self.app(self.client_id, self.client_secret, self.playlist_link)
+        
 
-playlist = "0m0xz4tyUyhQuJXQEwtyWq" #Replace with your playlist ID
 
-client_id = os.environ["CLIENT_ID"] #Replace with your Spotify Client ID
-client_secret = os.environ["CLIENT_SECRET"] # Replace with your Spotify CLient Secret
-developer_key = os.environ["YOUTUBE_API_KEY"] # Replace with your YouTube API Key
-
-
-def app(client_id, client_secret, playlist_id, youtube_id):
-    spotify.get_access_token(client_id, client_secret, playlist_id)
-    
-    youtube_id.get_id(developer_key, spotify.my_songs)
-
-app(client_id, client_secret, playlist, youtube_id)
+    def app(self, client_id, client_secret, playlist_link):
+        first = "https://open.spotify.com/playlist/"
+        last = "?si="
+        
+        start = playlist_link.index(first) + len(first)
+        end = playlist_link.index(last, start)
+        playlist_id = playlist_link[start:end]
+        self.spotify.get_access_token(client_id, client_secret, playlist_id)
+        self.youtube_id.get_id(developer_key=self.developer_key, song_names=self.spotify.song_names)
+        
+app = App()
--- youtube_id.py
+++ youtube_id.py
@@ -1,6 +1,5 @@
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from dotenv import load_dotenv
 import itertools
 
 class YoutubeId:
@@ -9,19 +8,22 @@ class YoutubeId:
         self.video_id = []
 
     def get_id(self, developer_key, song_names):
-        YOUTUBE_API_SERVICE_NAME = "youtube"
-        YOUTUBE_API_VERSION = "v3"
-        youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=developer_key)
+        try:
+            YOUTUBE_API_SERVICE_NAME = "youtube"
+            YOUTUBE_API_VERSION = "v3"
+            youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=developer_key)
 
-        for song in itertools.islice(song_names, 2):
-        # for song in song_names:
-            search_response = youtube.search().list(
-                q=song,
-                type="video",
-                part="id,snippet",
-                maxResults=1
-            ).execute()
-            
-            self.video_id.append(search_response["items"][0]["id"]["videoId"])
-        print(self.video_id)
-        return self.video_id
+            for song in itertools.islice(song_names, 2):
+            # for song in song_names:
+                search_response = youtube.search().list(
+                    q=song,
+                    type="video",
+                    part="id,snippet",
+                    maxResults=1
+                ).execute()
+                
+                self.video_id.append(search_response["items"][0]["id"]["videoId"])
+            print(self.video_id)
+            return self.video_id
+        except HttpError:
+            return ""
